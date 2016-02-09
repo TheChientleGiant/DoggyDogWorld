5fe2dadaebd41b1901abed57cf85c8cccb944b48
TheChientleGiant <thechientlegiant@gmail.com>
2016-02-09 13:54:34 -0800

    added x = 1, log value of x

--- DoggyDogWorld/app/src/main/java/com/shomiimages/doggydogworld/MainActivity.java
+++ DoggyDogWorld/app/src/main/java/com/shomiimages/doggydogworld/MainActivity.java
@@ -17,19 +17,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("MainActivity", "We're logging!!!");
+        int x = 1;
+        Log.d("MainActivity", "x is "+x);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
